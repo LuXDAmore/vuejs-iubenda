1d4ec6841f9dc532adb9dc7ab0d64d31f65047d7
Luca Iaconelli <lucabelli@msn.com>
2025-05-30 18:01:28 +0200

    feat: main title component

---
+++ src/components/MainTitle/MainTitle.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="main-title">
+        <h1
+            v-if="msg"
+            class="green"
+            v-text="msg"
+        />
+        <h3>
+            Project created with<br>
+            <a
+                href="https://vite.dev/"
+                rel="noopener"
+                target="_blank"
+            >
+                Vite
+            </a> +
+            <a
+                href="https://vuejs.org/"
+                rel="noopener"
+                target="_blank"
+            >
+                Vue 3
+            </a>
+        </h3>
+    </div>
+</template>
+
+<script setup>
+
+    defineProps(
+        {
+            msg: {
+                type: String,
+                default: '',
+            },
+        }
+    );
+
+</script>
+
+<style
+    scoped
+    lang="scss"
+    src="./MainTitle.scss"
+></style>
