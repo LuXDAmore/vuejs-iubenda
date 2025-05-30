07755f992127c0095cc1ce1de1098c55b55fb98c
Luca Iaconelli <lucabelli@msn.com>
2025-05-30 18:01:48 +0200

    feat: pinia store

---
+++ src/stores/counter.js
@@ -0,0 +1,29 @@
+import { computed, ref } from 'vue';
+import { defineStore } from 'pinia';
+
+// Store
+export const useCounterStore = defineStore(
+    'counter',
+    () => {
+
+        const count = ref( 0 )
+            , doubleCount = computed( () => count.value * 2 )
+        ;
+
+        /**
+         * Increment
+         */
+        function increment() {
+
+            count.value ++;
+
+        }
+
+        return {
+            count,
+            doubleCount,
+            increment
+        };
+
+    }
+);
