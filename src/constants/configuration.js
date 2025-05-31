b96a2e7b6d96e43c08885fb12e162654997c8083
Luca Iaconelli <lucabelli@msn.com>
2025-05-31 15:37:17 +0200

    chore: added constants

---
+++ src/constants/configuration.js
@@ -0,0 +1,18 @@
+export const CONFIGURATION_DEFAULT = {
+
+    banner: {
+        acceptButtonDisplay: false, // visibility of the Accept button
+        closeButtonDisplay: true, // visibility of the Close button
+        closeButtonRejects: false, // clicking the Close button should accept (`false`) or reject (`true`) the consent
+        rejectButtonDisplay: false, // visibility of the Reject button
+        title: 'Notice', // Text field that will change the displayed title of the banner
+    },
+
+    ccpa: false,
+    consentByScroll: false,
+
+    gdpr: true,
+    perPurposeConsent: false,
+    targetCountries: 'EU', // Possible values: 'EU', 'US', 'world'
+
+};
