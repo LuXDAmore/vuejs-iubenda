fb96e28bb1b0b5608f5961ba69ef496429404e72
Luca Iaconelli <lucabelli@msn.com>
2025-05-31 19:01:18 +0200

    feat: added about the project page with instructions

--- src/views/AboutView/AboutView.vue
+++ src/views/AboutView/AboutView.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="about-view">
-        <h1>This is an about page</h1>
+        <div>
+            <h2>About the project</h2>
+            <em>Luca Iaconelli</em><br>
+            <strong>Iubenda Challenge - Vuejs</strong><br><br>
+            <p>
+                Navigate to the `/challenge` page and play with the form-fields, then save the data when you want to.<br>
+                The data is saved into the store, and the form can be saved again only after updating fields again.<br><br>
+
+                If the user clicks one of the CookieBanner's buttons, the results are also saved into the store and can be viewed in the "PiniaStore accordion [Show Pinia store state]".<br>
+                There is also a "Json accordion [Show Json]" to show how the values of the form changes during user interactions.
+            </p>
+        </div>
     </div>
 </template>
 
