46208f7eee941bcbeb7206253cbab3d7f6e65e2f
Luca Iaconelli <lucabelli@msn.com>
2025-05-31 16:04:49 +0200

    feat: challenge view

--- src/views/ChallengeView/ChallengeView.vue
+++ src/views/ChallengeView/ChallengeView.vue
@@ -1,8 +1,247 @@
 <template>
     <section class="challenge-view">
-        PROVa
+
+        <h4 class="title">
+            CHALLENGE
+        </h4>
+
+        <form
+            action="/"
+            autocomplete="off"
+            method="post"
+            novalidate
+            @keydown.prevent.enter="() => onFormSubmitted()"
+            @keypress.prevent.enter="() => onFormSubmitted()"
+            @keyup.prevent.enter="() => onFormSubmitted()"
+            @submit.prevent="() => onFormSubmitted()"
+        >
+
+            <fieldset>
+                <h5 class="field-title">
+                    Target countries
+                </h5>
+
+                <div class="field">
+                    <input
+                        v-model="data.targetCountries"
+                        id="target-countries-worldwide"
+                        name="target-countries"
+                        type="radio"
+                        value="world"
+                    >
+                    <label for="target-countries-worldwide">
+                        Worldwide
+                    </label>
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.targetCountries"
+                        id="target-countries-us"
+                        name="target-countries"
+                        type="radio"
+                        value="US"
+                    >
+                    <label for="target-countries-us">
+                        US users only
+                    </label>
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.targetCountries"
+                        id="target-countries-eu"
+                        name="target-countries"
+                        type="radio"
+                        value="EU"
+                    >
+                    <label for="target-countries-eu">
+                        EU users only
+                    </label>
+                </div>
+
+            </fieldset>
+
+            <fieldset>
+
+                <h5 class="field-title">
+                    Legislation
+                </h5>
+
+                <div class="field">
+                    <input
+                        v-model="data.gdpr"
+                        id="legislation-gdpr"
+                        checked="checked"
+                        name="gdpr"
+                        type="checkbox"
+                    >
+                    <label for="legislation-gdpr">
+                        GDPR
+                    </label>
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.ccpa"
+                        id="legislation-ccpa"
+                        name="ccpa"
+                        type="checkbox"
+                    >
+                    <label for="legislation-ccpa">
+                        CCPA
+                    </label>
+                </div>
+
+            </fieldset>
+
+            <fieldset>
+
+                <h5 class="field-title">
+                    Consent
+                </h5>
+
+                <div class="field">
+                    <input
+                        v-model="data.consentByScroll"
+                        id="consent-by-scroll"
+                        name="consent-by-scroll"
+                        type="checkbox"
+                    >
+                    <label for="consent-by-scroll">
+                        Consent by scroll
+                    </label>
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.perPurposeConsent"
+                        id="per-purpose-consent"
+                        name="per-purpose-consent"
+                        type="checkbox"
+                    >
+                    <label for="per-purpose-consent">
+                        Consent by scroll
+                    </label>
+                </div>
+
+            </fieldset>
+
+            <hr>
+
+            <fieldset class="field-container__banner">
+
+                <h5 class="field-title">
+                    Banner
+                </h5>
+
+                <div class="field field-notice">
+                    <label for="banner-notice">
+                        Title
+                    </label>
+                    <input
+                        v-model="data.banner.title"
+                        id="banner-notice"
+                        name="banner-notice"
+                        type="text"
+                    >
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.banner.acceptButtonDisplay"
+                        id="banner-accept-button-display"
+                        name="banner-accept-button-display"
+                        type="checkbox"
+                    >
+                    <label for="banner-accept-button-display">
+                        Accept button display
+                    </label>
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.banner.closeButtonDisplay"
+                        id="banner-close-button-display"
+                        name="banner-close-button-display"
+                        type="checkbox"
+                    >
+                    <label for="banner-close-button-display">
+                        Close button display
+                    </label>
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.banner.closeButtonRejects"
+                        id="banner-close-button-rejects"
+                        name="banner-close-button-rejects"
+                        type="checkbox"
+                    >
+                    <label for="banner-close-button-rejects">
+                        Close button rejects
+                    </label>
+                </div>
+
+                <div class="field">
+                    <input
+                        v-model="data.banner.rejectButtonDisplay"
+                        id="banner-reject-button-display"
+                        name="banner-reject-button-display"
+                        type="checkbox"
+                    >
+                    <label for="banner-reject-button-display">
+                        Reject button display
+                    </label>
+                </div>
+
+            </fieldset>
+
+            <hr>
+
+            <button class="field-submit" type="submit">
+                Save
+            </button>
+
+        </form>
+
+        <cookie-banner :data="data" />
+
+        <details>
+            <summary>Show Json</summary>
+            <code>
+                <pre v-text="prettifiedJsonToShowInUi" />
+            </code>
+        </details>
+
     </section>
 </template>
+
+<script setup>
+
+    // Vue
+    import { computed, ref } from 'vue';
+
+    // Components
+    import CookieBanner from '@/components/CookieBanner/CookieBanner.vue';
+
+    // Constants
+    import { CONFIGURATION_DEFAULT } from '@/constants/configuration';
+
+    // Setup
+    const
+        // Configuration
+        data = ref( CONFIGURATION_DEFAULT )
+        , prettifiedJsonToShowInUi = computed( () => JSON.stringify( data.value, null, 2 ) )
+        // Events
+        , onFormSubmitted = () => {
+
+            console.info( data.value );
+
+        }
+    ;
+
+</script>
 
 <style
     scoped
